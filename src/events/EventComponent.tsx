6add718806dc348c3f424cfb4cc7e3fa841a9acd
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2021-12-13 12:54:13 +0530

    added notes about event objects
    added notes about event objects

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const EventComponent: React.FC = () => {
+    
+    //Events supported by React are : Clipboard Events Composition Events,Keyboard Events, Focus Events, Form Events, Generic Events,Mouse Events, Pointer Events,    Selection Events, Touch Events, UI Events, Wheel Events, Media Events, Image Events, Animation Events, Transition Events, Other Events
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value);
     };
--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//An interface is a syntactical contract that an entity should conform to. In other words, an interface defines the syntax that any entity must adhere to.
+//we defines the types of values passed in props here in interface
 interface ChildProps {
     color: string;
     onClick: () => void;
 }
 
+//here we are using interface to pass the types info by using :ChildProps
 export const Child = ({ color, onClick }: ChildProps) => {
     return (
         <div>
