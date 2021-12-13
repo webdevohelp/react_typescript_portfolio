e98154b7b5c95d400f035be78e52bd55499ae6a7
Web Dev Help <43485053+webdevohelp@users.noreply.github.com>
2021-12-13 12:16:29 +0530

    added types to refs
    added types to refs

--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -10,8 +10,11 @@ const UserSearch: React.FC = () => {
         { name: 'cat', age: 20 },
         { name: 'dog', age: 10 },
     ];
-
+    // to set type of ref we have to trll typescript about the element to which we are going to attach ref
+    // here useRef is connected to input element so we set type to HTMLInputELement
+    // to see the list of elements ctrl click on HTMLInputElement and see the list
     const inputRef = useRef<HTMLInputElement | null>(null);
+
     const [name, setName] = useState<string>('');
     const [user, setUser] = useState<
         { name: string; age: number } | undefined
